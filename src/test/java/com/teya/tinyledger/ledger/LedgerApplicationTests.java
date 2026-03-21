5d338980cb354d386c35be809535f116c970dd22
William Cisotto <willgalindo@gmail.com>
2026-03-21 14:04:42 +0000

    first commit

---
+++ src/test/java/com/teya/tinyledger/ledger/LedgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teya.tinyledger.ledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
